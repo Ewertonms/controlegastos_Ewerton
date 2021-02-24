142a936c5692afd73f0df9684f545db2f159fc7a
Ewerton M <56974361+Ewertonms@users.noreply.github.com>
2021-02-23 23:16:44 -0300

    criação de nova view listar_gastos

---
+++ application/views/Listar_gastos.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+</head>
+<body>
+  <table>
+    <tr><td>descrição</td><td>valor</td></tr>
+    <tr><td>Apostilas</td><td>350,00</td></tr>
+    <tr><td>transporte</td><td>500,00</td></tr>
+  </td>
+
+</body>
+</html>
