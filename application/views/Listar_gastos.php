15ac5e31138ef4fe4640a06c049e9e54c1c19645
Ewerton M <56974361+Ewertonms@users.noreply.github.com>
2021-02-23 23:39:20 -0300

    Update Listar_gastos.php

--- application/views/Listar_gastos.php
+++ application/views/Listar_gastos.php
@@ -8,6 +8,6 @@
     <tr><td>descrição</td><td>valor</td></tr>
     <tr><td>Apostilas</td><td>350,00</td></tr>
     <tr><td>transporte</td><td>500,00</td></tr>
-	</table>
+   </table>
 </body>
 </html>
